b4c87cc80fec8663f1896883eb9428be62077d60
landmanbester <lbester@sarao.ac.za>
2026-04-15 15:40:35 +0200

    build: restructure dependencies and remove unnecessary ones

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,22 +7,22 @@ test_root_path = Path(__file__).resolve().parent
 test_data_path = Path(test_root_path, "data")
 test_data_path.mkdir(parents=True, exist_ok=True)
 
-_data_tar_name = "MeerKAT_UHF.zarr.tgz"
-_beam_name = "MeerKAT_UHF.zarr"
+_data_tar_name = "MeerKAT_U.zarr.tgz"
+_beam_name = "MeerKAT_U.zarr"
 
 data_tar_path = Path(test_data_path, _data_tar_name)
 beam_path = Path(test_data_path, _beam_name)
 
 # Band code -> input zarr filename in tests/data/
 BAND_INPUT_ZARR = {
-    "U": "MeerKAT_UHF.zarr",
+    "U": "MeerKAT_U.zarr",
     "L": "MeerKAT_L.zarr",
     # S-band sub-bands can be added as data becomes available
-    # "S0": "MeerKAT_S0.zarr",
+    "S0": "MeerKAT_S0.zarr",
     # "S1": "MeerKAT_S1.zarr",
     # "S2": "MeerKAT_S2.zarr",
     # "S3": "MeerKAT_S3.zarr",
-    # "S4": "MeerKAT_S4.zarr",
+    "S4": "MeerKAT_S4.zarr",
 }
 
 # https://drive.google.com/file/d/13k5WyyQFdcNG8FqsBAz3mAvhuaVZ2FlW/view?usp=sharing
