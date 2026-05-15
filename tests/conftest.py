d390904f8c13c2d01529556b2ef5fe284f5182ec
landmanbester <lbester@sarao.ac.za>
2026-05-15 16:46:11 +0200

    test(conftest): expose PKS 1934-638 spectrum as CALIBRATOR_SPECTRUM dict

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,22 @@ test_data_path.mkdir(parents=True, exist_ok=True)
 # https://drive.google.com/file/d/1mCTrC3IbMUqu0Adu1DWOjhwvzQS6gseo/view?usp=drive_link
 test_ms_gdrive_id = "1mCTrC3IbMUqu0Adu1DWOjhwvzQS6gseo"
 
+# Primary calibrator location
+ra = "19:39:25.027"
+dec = "-63.42.45.626"
+
+# Primary calibrator (PKS 1934-638) spectral model.
+# I(nu) = I0 * (nu/nu0) ** (a + b*x + c*x**2 + d*x**3 + e*x**4)   where x = log10(nu/nu0)
+CALIBRATOR_SPECTRUM = {
+    "I0": 15.088731791006047,
+    "nu0": 1283791015.625,
+    "a": -1.2369319597991164,
+    "b": -7.995603882017982,
+    "c": 11.605973123430397,
+    "d": -15.787559501497967,
+    "e": -3.928824456855068,
+}
+
 
 def pytest_sessionstart(session):
     """Populate the L-band cache once per session if it isn't there yet."""
